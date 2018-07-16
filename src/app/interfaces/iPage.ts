c50cb1460a5d2c610bba889dff9731c8121e16e7
Eldar Khaitov <e.khaitov@andersenlab.com>
2018-07-16 12:03:48 +0300

    Fixed IPage interface

--- src/app/interfaces/iPage.ts
+++ src/app/interfaces/iPage.ts
@@ -1,8 +1,9 @@
-import {INavigationItem} from "./iNavigation";
-import {IContact} from "./iContact";
+import {INavigationItem} from './iNavigation';
+import {IContact} from './iContact';
 
 export default interface IPage {
-  header?: string;
+  header: string;
   body?: string | Array<INavigationItem> | Array<IContact>;
-  entityId?: string;
+  entityId: 'nav' | 'contacts' | 'about' | 'diploma' | 'service' | 'article';
+  language: 'en' | 'ru' | 'uk';
 }
