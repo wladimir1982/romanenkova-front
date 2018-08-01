9169497512d41e2f31b10cf9ebd4209c1ab1dfe8
Eldar Khaitov <e.khaitov@andersenlab.com>
2018-08-01 22:07:13 +0300

    About page wireframe

--- src/app/pages/about/components/about/about.component.ts
+++ src/app/pages/about/components/about/about.component.ts
@@ -7,11 +7,13 @@ import {ActivatedRoute} from "@angular/router";
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
-  public mainText: string;
+  public mainText: Array<string>;
+  public photos: Array<string>;
+
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.mainText = this.route.snapshot.data.pageData.pageData;
+    this.mainText = this.route.snapshot.data.pageData.pageData.split('\n').filter((s: string) => Boolean(s));
+    this.photos = this.route.snapshot.data.pageData.images;
   }
-
 }
