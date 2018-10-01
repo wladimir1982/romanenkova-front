c85b4a4979a2accc3a3efed9ac586008f76df96f
Eldar Khaitov <e.khaitov@andersenlab.com>
2018-10-01 19:22:29 +0300

    Created custom datepicker

--- src/app/components/date-input/date-input.component.ts
+++ src/app/components/date-input/date-input.component.ts
@@ -3,6 +3,11 @@ import {INgxMyDpOptions, NgxMyDatePickerConfig, NgxMyDatePickerDirective, IMyInp
 import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
 import {Observable} from 'rxjs';
 import {filter} from 'rxjs/operators';
+import * as Moment from 'moment';
+import {extendMoment} from 'moment-range';
+import {MomentObjectOutput} from 'moment';
+
+const moment = extendMoment(Moment);
 
 export const CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR: any = {
   provide: NG_VALUE_ACCESSOR,
@@ -22,25 +27,28 @@ export class DateInputComponent implements ControlValueAccessor, OnInit {
   @Input() options: any;
   @Input() icon?: string;
   @Input() events$: Observable<Event>;
-  @ViewChild('dp') dp: NgxMyDatePickerDirective;
 
   private isDisabled: boolean;
   private isCalendarOpen: boolean;
-  private currentDate: Date = new Date();
-  public value: any;
-  public dpOptions: INgxMyDpOptions = {
-    dateFormat: 'dd mmm yyyy',
-    disableUntil: {year: this.currentDate.getFullYear(), month: this.currentDate.getMonth() + 1, day: this.currentDate.getDate()},
-    disableSince: {year: this.currentDate.getFullYear(), month: this.currentDate.getMonth() + 1, day: this.currentDate.getDate() + 28},
-    enableDates: [{year: this.currentDate.getFullYear(), month: this.currentDate.getMonth() + 1, day: this.currentDate.getDate()}],
-    disableWeekdays: ['su'],
-    allowSelectionOnlyInCurrentMonth: false,
-    showTodayBtn: false,
-    showSelectorArrow: false
+  public selectedDate: Moment.Moment = moment().startOf('day');
+  public minDate: Moment.Moment = moment().startOf('day');
+  private range = moment.rangeFromInterval('days', 600, this.minDate);
+  public monthYearRange: Array<Moment.Moment>;
+  public monthYearRangeIndex: number = 0;
+  public weekdays = {
+    mo: 'mo',
+    tu: 'tu',
+    we: 'we',
+    th: 'th',
+    fr: 'fr',
+    st: 'st',
+    su: 'su',
   };
+  public value: any;
   public textValue: string;
   private onTouch: () => void;
-  private onChange: (v: string) => void = () => {};
+  private onChange: (v: string) => void = () => {
+  };
 
   constructor(private changeDetectorRef: ChangeDetectorRef) {
   }
@@ -63,11 +71,13 @@ export class DateInputComponent implements ControlValueAccessor, OnInit {
   }
 
   public ngOnInit(): void {
+    this.monthYearRange = Array.from(this.range.by('month'));
+
     this.events$.pipe(
       filter((e: Event): boolean => e.type === 'click')
     )
       .subscribe((e: Event) => {
-        this.toggleCalendar(this.dp, false);
+        // this.toggleCalendar(this.dp, false);
       });
   }
 
@@ -90,7 +100,24 @@ export class DateInputComponent implements ControlValueAccessor, OnInit {
     $event.stopPropagation();
   }
 
-  onBlur($event: FocusEvent) {
-    console.log($event, this.value);
+  public setMonth(direction: string): void {
+    if ((this.monthYearRangeIndex === 0 && direction === 'prev') ||
+      this.monthYearRangeIndex === (this.monthYearRange.length - 1) && direction === 'next') {
+      return;
+    }
+
+    const addvalue = direction === 'next' ? 1 : -1;
+    this.monthYearRangeIndex += addvalue;
+  }
+
+  public getDaysLine(): Array<Moment.Moment> {
+    return Array.from(this.range.by('day'))
+      .filter((d: Moment.Moment): boolean =>
+        (d.month() === this.monthYearRange[this.monthYearRangeIndex].month()) &&
+        (d.year() === this.monthYearRange[this.monthYearRangeIndex].year()));
+  }
+
+  setActiveDate(day: Moment.Moment) {
+    this.selectedDate = day;
   }
 }
